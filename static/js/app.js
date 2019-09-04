522ebec47746da1c856b447b302ae9a85f6744e8
unknown <cienfuegos.gc@gmail.com>
2019-09-03 22:48:03 -0500

    Part 1 completed, functional filter and results printing

---
+++ static/js/app.js
@@ -0,0 +1,34 @@
+// Assign the data from `data.js` to a descriptive variable
+var ufo = data;
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  // Filter the data with the timedate provided by the user
+  var filteredData = ufo.filter(ufo => ufo.datetime === inputValue);
+  console.log(filteredData);
+
+  // Then, select the unordered list element by sub anchor "tbody"
+  var tbody = d3.select("table>tbody")
+
+  // remove any children from the list to
+  tbody.html("");
+
+  // Populate the table, appending first rows, then each cell
+  filteredData.forEach((filteredData) => {
+    var row = tbody.append("tr");
+    Object.entries(filteredData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value)
+    });
+  });
+  
+});
